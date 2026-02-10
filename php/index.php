14848cfaafacf8c9414a0fe6e8440afd3a7a1cae
Thennee <101799630+Thennee@users.noreply.github.com>
2026-02-10 20:08:25 +0700

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,9 @@
+<?php
+  /* Kode di bawah ini sedang diperbaiki,
+     jadi disembunyikan dulu sementara:
+     $total = $harga + $pajak;
+     echo $total;
+  */
+
+  echo "Sistem sedang dalam pemeliharaan.";
+?>
